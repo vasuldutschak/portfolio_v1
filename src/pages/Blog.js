043115072a1ef828ec2890254e995569c373b701
vasuldutschak <vasuldutschak@mail.ru>
2023-03-19 22:04:14 +0200

    make scroll to start to navigate on blog page

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import BlogCard from "../components/sections/blog/BlogCard";
 import img1 from "../blog1.jpg";
 import img2 from "../blog2.jpg";
 
 function Blog(props) {
     const text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi venenatis et tortor ac tincidunt. In euismod iaculis lobortis. Vestibulum posuere molestie ipsum vel...'
-
+    useEffect(()=>{
+        window.scroll({behavior:'smooth',top:0})
+    },[])
     return (
         <section className='blog' id='blog'>
             <div className="blog__container">
@@ -17,13 +19,13 @@ function Blog(props) {
                     <BlogCard img={img2} hasTags={['#branding', '#ui-ux']} title='Music Player Design' text={text}
                               date={{day: 17, dayName: "July"}}>
                     </BlogCard>
-                <BlogCard img={img1} hasTags={['#branding', '#ui-ux']} title='Music Player Design' text={text}
+                    <BlogCard img={img1} hasTags={['#branding', '#ui-ux']} title='Music Player Design' text={text}
                               date={{day: 27, dayName: "July"}}>
                     </BlogCard>
                     <BlogCard img={img2} hasTags={['#branding', '#ui-ux']} title='Music Player Design' text={text}
                               date={{day: 17, dayName: "July"}}>
                     </BlogCard>
-                <BlogCard img={img1} hasTags={['#branding', '#ui-ux']} title='Music Player Design' text={text}
+                    <BlogCard img={img1} hasTags={['#branding', '#ui-ux']} title='Music Player Design' text={text}
                               date={{day: 27, dayName: "July"}}>
                     </BlogCard>
                     <BlogCard img={img2} hasTags={['#branding', '#ui-ux']} title='Music Player Design' text={text}
