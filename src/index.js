bfc817a49d6904bdf8e57982052bf30c62f8754d
vasuldutschak <vasuldutschak@mail.ru>
2023-03-24 21:08:22 +0100

    add basename

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {BrowserRouter} from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename="portfolio_v1">
             <App/>
         </BrowserRouter>
     </React.StrictMode>
